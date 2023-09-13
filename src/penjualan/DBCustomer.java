d25b9c95806a03b6d54a937ec217788d54c7533a
Nathanael Ferry P <ntnferry1505@gmail.com>
2023-09-13 16:12:59 +0700

    Penjualan

--- src/penjualan/DBCustomer.java
+++ src/penjualan/DBCustomer.java
@@ -1,6 +1,7 @@
 package penjualan;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -44,5 +45,76 @@ public class DBCustomer {
             return null;
         }
     }
-
+    public int validasi(String nomor) {
+        int val = 0;
+        try {         
+            Koneksi con = new Koneksi();            
+            con.bukaKoneksi();
+            con.statement = con.dbKoneksi.createStatement();
+            ResultSet rs = con.statement.executeQuery(  "select count(*) as jml from customer where idmember = '" + nomor + "'");
+            while (rs.next()) {                
+                val = rs.getInt("jml");            
+            }            
+            con.tutupKoneksi();
+        } catch (SQLException e) {            
+            e.printStackTrace();        
+        }
+        return val;
+    }
+    public boolean insert() {
+        boolean berhasil = false;        
+        Koneksi con = new Koneksi();
+        try {       
+            con.bukaKoneksi();
+            con.preparedStatement = con.dbKoneksi.prepareStatement("insert into customer (idmember,nama, alamat, totalbelanja) values (?,?,?,?)");
+            con.preparedStatement.setString(1, getCustModel().getId());           
+            con.preparedStatement.setString(2, getCustModel().getNama());
+            con.preparedStatement.setString(3, getCustModel().getAlamat());           
+            con.preparedStatement.setDouble(4, getCustModel().getTotal());        
+            con.preparedStatement.executeUpdate();
+            berhasil = true;
+        } catch (Exception e) {            
+            e.printStackTrace();            
+            berhasil = false;
+        } finally {            
+            con.tutupKoneksi();            
+            return berhasil;        
+        }    
+     }
+public boolean delete(String nomor) {
+        boolean berhasil = false;        
+        Koneksi con = new Koneksi();
+        try {            
+            con.bukaKoneksi();;
+            con.preparedStatement = con.dbKoneksi.prepareStatement("delete from customer where idmember  = ? ");
+            con.preparedStatement.setString(1, nomor);
+            con.preparedStatement.executeUpdate();            
+            berhasil = true;
+        } catch (Exception e) {            
+            e.printStackTrace();
+        } finally {            
+            con.tutupKoneksi();            
+            return berhasil;        
+        }
+    }
+public boolean update() {
+        boolean berhasil = false;        
+        Koneksi con = new Koneksi();
+        try {            
+            con.bukaKoneksi();
+            con.preparedStatement = con.dbKoneksi.prepareStatement("update customer set nama = ?, alamat = ?, totalbelanja = ?  where  idmember = ? ");
+            con.preparedStatement.setString(1, getCustModel().getNama());
+            con.preparedStatement.setString(2, getCustModel().getAlamat());
+            con.preparedStatement.setDouble(3, getCustModel().getTotal());
+            con.preparedStatement.setString(4, getCustModel().getId());
+            con.preparedStatement.executeUpdate();            
+            berhasil = true;
+        } catch (Exception e) {            
+            e.printStackTrace();            
+            berhasil = false;
+        } finally {            
+            con.tutupKoneksi();            
+            return berhasil;        
+        }    
+        }
 }
