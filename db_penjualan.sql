d74dd16c729401da509b924ed3233356e216034b
Nathanael Ferry P <ntnferry1505@gmail.com>
2023-09-13 15:20:24 +0700

    Update Database

--- db_penjualan.sql
+++ db_penjualan.sql
@@ -14,7 +14,33 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for db_jual
+DROP DATABASE IF EXISTS `db_jual`;
+CREATE DATABASE IF NOT EXISTS `db_jual` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_jual`;
+
+-- Dumping structure for table db_jual.barang
+DROP TABLE IF EXISTS `barang`;
+CREATE TABLE IF NOT EXISTS `barang` (
+  `kodebrg` varchar(50) NOT NULL,
+  `namabrg` varchar(50) DEFAULT NULL,
+  `tarif` double DEFAULT NULL,
+  `gambar` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`kodebrg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Data exporting was unselected.
+
+-- Dumping structure for table db_jual.customer
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `idmember` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  PRIMARY KEY (`idmember`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
